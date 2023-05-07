9ce114547c5bcb44d8233b632ea040d0f1e2126d
ShinHeeJin <sisesu92@gmail.com>
2023-05-08 02:00:03 +0900

    feat : clientApp 생성 및 Test 코드 작성

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // 인터페이스와 구현제 모두 의존 DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
 
