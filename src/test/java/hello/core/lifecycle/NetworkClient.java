ffddd918198d35bf4175d8ff69557048eec714c0
ShinHeeJin <sisesu92@gmail.com>
2023-05-17 15:39:07 +0900

    feat : javax.PostConstruct & PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,6 @@
 package hello.core.lifecycle;
-
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class NetworkClient {
 
@@ -15,6 +16,7 @@ public class NetworkClient {
         this.url = url;
     }
 
+    @PostConstruct
     public void connect(){
         System.out.println("NetworkClient.connect");
         System.out.println("connect = " + url);
@@ -25,21 +27,9 @@ public class NetworkClient {
         System.out.println("url = " + url + "message = " + message);
     }
 
+    @PreDestroy
     public void disconnect(){
         System.out.println("NetworkClient.disconnect");
         System.out.println("close = " + url);
-    }
-
-    public void init() {
-        // 스프링 의존관계 주입 모두 끝난 후 호출됨
-        System.out.println("NetworkClient.init");
-        connect();
-        call("초기화 연결 메시지");
-    }
-
-    public void close() {
-        // 스프링 빈이 소멸될 때 호출됨
-        System.out.println("NetworkClient.close");
-        disconnect();
     }
 }
